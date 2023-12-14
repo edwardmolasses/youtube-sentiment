f2c13896674130094ac2db1e550f7a18a0ef76d5
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2023-12-14 14:48:23 -0800

    move youtube fns to library

---
+++ libraries/youtube-utils.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+async function fetchChannelId(channelName) {
+    const channelIdApiUrl = `https://www.googleapis.com/youtube/v3/channels?part=id&forUsername=${channelName}&key=${process.env.GOOGLE_API}`;
+
+    try {
+        const response = await axios.get(channelIdApiUrl);
+        return response.data.items[0].id;
+    } catch (error) {
+        // Handle errors
+        console.error('Error fetching data from YouTube API:', error.message);
+    }
+}
+
+async function fetchChannelVideos(channelId) {
+    const videoListApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=5`;
+    try {
+        const response = await axios.get(videoListApiUrl);
+        // Handle the response data, which contains information about the latest videos
+        return response.data.items;
+    } catch (error) {
+        // Handle errors
+        console.error('Error fetching data from YouTube API:', error.message);
+    }
+}
+
+module.exports = { fetchChannelId, fetchChannelVideos };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
+const { fetchChannelId, fetchChannelVideos } = require('./libraries/youtube-utils');
 const express = require('express');
 const path = require('path');
 const cron = require('node-cron');
 const cors = require('cors');
 const { google } = require('googleapis');
-const axios = require('axios');
 const app = express();
 const port = 3001;
 const cronInterval = 1;
@@ -29,30 +29,6 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-
-async function fetchChannelId(channelName) {
-    const channelIdApiUrl = `https://www.googleapis.com/youtube/v3/channels?part=id&forUsername=${channelName}&key=${process.env.GOOGLE_API}`;
-
-    try {
-        const response = await axios.get(channelIdApiUrl);
-        return response.data.items[0].id;
-    } catch (error) {
-        // Handle errors
-        console.error('Error fetching data from YouTube API:', error.message);
-    }
-}
-
-async function fetchChannelVideos(channelId) {
-    const videoListApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=5`;
-    try {
-        const response = await axios.get(videoListApiUrl);
-        // Handle the response data, which contains information about the latest videos
-        return response.data.items;
-    } catch (error) {
-        // Handle errors
-        console.error('Error fetching data from YouTube API:', error.message);
-    }
-}
 
 cron.schedule(`*/${cronInterval} * * * *`, async () => {
     console.log(`Running the script at ${cronInterval} minute intervals.`);
